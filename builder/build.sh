b73bfe5955a7a3f1e167949a2bca47571893b333
Artem Borovik <polzovatellllk@gmail.com>
2025-04-06 19:04:29 +0300

    bundle all webrogue commands in one binary

--- builder/build.sh
+++ builder/build.sh
@@ -4,6 +4,6 @@ sh ../libraries/copy_sources.sh
 
 run_in_docker "
 make -C /webrogue-sdk/libraries TOOLCHAIN=wasip1 1>/dev/null
-cargo build --release --manifest-path=/host_dir/crates/wrapp-cli/Cargo.toml --target-dir=/host_dir/webrogue-sdk/target
-cp /host_dir/webrogue-sdk/target/release/webrogue-wrapp-cli /opt/webrogue-wrapp-cli
+cargo build --release --manifest-path=/host_dir/Cargo.toml --no-default-features --features=pack --target-dir=/host_dir/webrogue-sdk/target
+cp /host_dir/webrogue-sdk/target/release/webrogue /opt/webrogue
 "
